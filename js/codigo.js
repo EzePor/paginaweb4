d04655d5764daf14937ef0888141b03a9c753bd5
EzePor <107148407+EzePor@users.noreply.github.com>
2022-12-12 20:21:51 -0300

    js

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+function ponerEnMayusculas(){
+    var yaeshoradeseleccionarlas=document.getElementById("sub");
+    yaeshoradeseleccionarlas.innerHTML="YA ES HORA DE SELECCIONARLAS...";
+    yaeshoradeseleccionarlas.style.background="white";
+}
+
+function fondo(){
+    var recuerdos=document.getElementById("logotipo");
+    recuerdos.innerHTML="RECUERDOS PARA TODA LA VIDA !!!";
+    recuerdos.style.background="blue";
+    recuerdos.style.fontSize="45px";
+}
